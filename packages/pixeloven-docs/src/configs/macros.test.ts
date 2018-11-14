262e986f1d146bdaef34b4daa922246bb677f61e
Brian Gebel <bgebel@gofundme.com>
2018-11-14 09:24:11 -0800

    Adding fake test for now

---
+++ packages/pixeloven-docs/src/configs/macros.test.ts
@@ -0,0 +1,9 @@
+import "jest";
+
+describe("macros", () => {
+    describe("importAsset", () => {
+        it("should import an asset", () => {
+            expect(0).toBeLessThanOrEqual(0);
+        });
+    });
+});
