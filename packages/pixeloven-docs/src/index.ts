e9f39772f1a790cd6552876362b76ef5cbcab37a
Brian Gebel <bgebel@gofundme.com>
2018-11-14 15:45:02 -0800

    Adding Readme to root

--- packages/pixeloven-docs/src/index.ts
+++ packages/pixeloven-docs/src/index.ts
@@ -1,4 +1,4 @@
 export * from "@storybook/react";
 export { default as Actions } from "@storybook/addon-actions";
 export { default as Knobs } from "@storybook/addon-knobs";
-export { default as Readme } from "storybook-readme";
+export * from "storybook-readme";
