012b74357522dba5a7b9fbf959b6d6f5a7002714
Brian Gebel <bgebel@gofundme.com>
2018-12-05 20:27:20 -0800

    Create empty test file

---
+++ packages/pixeloven-generators/src/plopfile.test.ts
@@ -0,0 +1,15 @@
+import "jest";
+import generator from "./plopfile";
+
+describe("@pixeloven/generators", () => {
+    describe("plopfile", () => {
+        describe("generators", () => {
+            it("should generate component", () => {
+                expect(1).toEqual(1);
+            });
+            it("should generate store", () => {
+                expect(1).toEqual(1);
+            });
+        });
+    });
+});
