53840c7c23f60138bd9e4a1d38d01e46aebf4a06
Brian Gebel <bgebel@gofundme.com>
2019-05-02 17:49:10 -0700

    Fix test

--- packages/pixeloven-commit-config/src/index.ts
+++ packages/pixeloven-commit-config/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * @todo Make a single configuration file for both commit lint and commit citizen
  */
-module.exports = {
+export default {
     rules: {
         "body-leading-blank": [1, "always"],
         "footer-leading-blank": [1, "always"],
