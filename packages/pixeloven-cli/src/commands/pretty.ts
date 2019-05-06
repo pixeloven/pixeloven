fe39fb209e05e605011faba33de25fa2c82fbd61
Brian Gebel <bgebel@gofundme.com>
2019-05-06 09:58:34 -0700

    Fixing arg list issues for lint and pretty

--- packages/pixeloven-cli/src/commands/pretty.ts
+++ packages/pixeloven-cli/src/commands/pretty.ts
@@ -1,10 +1,11 @@
+import { NodeInvalidArgumentException } from "@pixeloven/exceptions";
 import { PixelOvenRunContext } from "../types";
 
 export default {
     alias: ["--pretty", "-p"],
     name: "pretty",
     run: async (context: PixelOvenRunContext) => {
-        const { parameters, print, prettier, styleLint, tsLint } = context;
+        const { parameters, pixelOven, print, prettier, styleLint, tsLint } = context;
         /**
          * Process results
          * @param name
@@ -15,30 +16,53 @@ export default {
                 print.error(`${name} exited with status ${status}\n`);
                 process.exit(status);
             } else {
-                print.success(`Success! Looks a lot nicer now doesn't it?!\n`);
+                print.success(`\nSuccess! Looks a lot nicer now doesn't it?!\n`);
             }
             return status;
         };
-        /**
-         * @todo if first is not in switch then we don't want to slice here
-         */
-        const argList =
-            parameters.array && parameters.array.length
-                ? parameters.array.slice(1)
-                : [];
-        const prettierResults = await prettier(argList);
-        const prettierStatus = handle("Prettier", prettierResults.status);
+
+
+
         switch (parameters.first) {
             case "scss": {
-                const results = await styleLint(["--fix"].concat(argList));
-                return handle("Stylelint", results.status);
+                const argList = pixelOven.getArgList("scss", parameters, {
+                    offset: 1,
+                    type: "withOptions",
+                });
+                const prettierResults = await prettier(argList);
+                const prettierStatus = handle("Prettier", prettierResults.status);
+
+                const styleLintResults = await styleLint(["--fix"].concat(argList));
+                const styleLintStatus = handle("Stylelint", styleLintResults.status);
+                return prettierStatus + styleLintStatus;
             }
-            case "ts":
+            case "ts": {
+                const argList = pixelOven.getArgList("ts", parameters, {
+                    offset: 1,
+                    type: "withOptions",
+                });
+                const prettierResults = await prettier(argList);
+                const prettierStatus = handle("Prettier", prettierResults.status);
+
+                const tsLintResults = await tsLint(["--fix"].concat(argList));
+                const tsLintStatus = handle("TSLint", tsLintResults.status);
+                return prettierStatus + tsLintStatus;
+            }
             case "tsx": {
-                const results = await tsLint(["--fix"].concat(argList));
-                return handle("TSLint", results.status);
+                const argList = pixelOven.getArgList("tsx", parameters, {
+                    offset: 1,
+                    type: "withOptions",
+                });
+                const prettierResults = await prettier(argList);
+                const prettierStatus = handle("Prettier", prettierResults.status);
+
+                const tsLintResults = await tsLint(["--fix"].concat(argList));
+                const tsLintStatus = handle("TSLint", tsLintResults.status);
+                return prettierStatus + tsLintStatus;
+            }
+            default: {
+                throw new NodeInvalidArgumentException();
             }
         }
-        return prettierStatus;
     },
 };
