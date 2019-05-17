c8b8d386a757b89d21b10262266c7e8798a1d870
Brian Gebel <bgebel@gofundme.com>
2019-05-17 10:27:37 -0700

    fix: cleanup to push new alpha

--- packages/pixeloven-cz-simplified-changelog/src/modules.d.ts
+++ packages/pixeloven-cz-simplified-changelog/src/modules.d.ts
@@ -1,12 +1,4 @@
 /**
- * Declare file formats not covered by typescript automatically
- */
-declare module "*.svg";
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.md";
-
-/**
  * Declare packages not explicitly defined
  * @description Consult DefinitelyTyped before declaring below
  * http://definitelytyped.org/
