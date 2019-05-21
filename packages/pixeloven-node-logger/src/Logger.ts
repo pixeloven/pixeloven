ba608aa5214095706dcdcdc6b479a002d3c6309c
Brian Gebel <bgebel@gofundme.com>
2019-05-21 08:36:19 -0700

    chore: adding todos

--- packages/pixeloven-node-logger/src/Logger.ts
+++ packages/pixeloven-node-logger/src/Logger.ts
@@ -5,11 +5,37 @@ type Level = "error" | "info" | "warn";
 type Message = string | string[];
 
 /**
+ * @todo Add colors and what not
+ * @todo replace the print function in the cli with this one
+ * @todo can we replace storybooks logger? or make our looks similar
+ * 
+ * @todo we can extend the AbstractConfigSetLevels and others for specific functionality
+ * @todo we want our logger to look good!
+ */
+const customConsoleFormat = winston.format.printf(info => {
+    return `${info.level.toUpperCase()}: ${info.meta.timestamp} ${
+        info.message
+    }`;
+});
+
+/**
  * Standard logger options shared by both our middleware and our logger endpoint
  */
 const loggerOptions = {
+    levels: {
+        error: 0,
+        info: 1,
+        success: 2,
+        warn: 3
+    },
     transports: [
-        new winston.transports.Console()
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.metadata({ key: "meta" }),
+                customConsoleFormat,
+            ),
+        })
     ],
 };
 
@@ -37,6 +63,8 @@ function log(level: Level, msg: Message) {
 
 /**
  * Simple wrapper for winston
+ * 
+ * @todo Need to add success but it's not a given type for winston so we need to extend it.
  */
 const Logger = {
     error: (msg: Message) => log("error", msg),
