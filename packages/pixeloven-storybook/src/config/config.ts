73f200d8fb214902cc6814019f29792749958ee8
James Wilson <jimmynotjim@me.com>
2019-07-11 15:26:05 -0700

    fix: convert existing storybook config to latest

--- packages/pixeloven-storybook/src/config/config.ts
+++ packages/pixeloven-storybook/src/config/config.ts
@@ -1,7 +1,5 @@
-import { withBackgrounds } from "@storybook/addon-backgrounds";
 import { withKnobs } from "@storybook/addon-knobs";
-import { withOptions } from "@storybook/addon-options";
-import { addDecorator, configure } from "@storybook/react";
+import { addDecorator, addParameters, configure } from "@storybook/react";
 
 /**
  * Import remote assets dynamically
@@ -17,23 +15,22 @@ import("" + "@src/shared/styles").catch((error: Error) => {
  * Setup storybook addons
  * @todo options in menu are overriden after you do anything how can we prevent this?
  */
-addDecorator(
-    withOptions({
-        addonPanelInRight: true,
-        goFullScreen: false,
-        name: "Storybook React",
+addParameters({ options: {
+        isFullScreen: false,
+        panelPosition: 'right',
         showSearchBox: false,
-    }),
-);
-addDecorator(
-    withBackgrounds([
-        { name: "default", value: "transparent", default: true },
-        { name: "#000", value: "#000000", default: false },
-        { name: "#333", value: "#333333", default: false },
-        { name: "twitter", value: "#00aced", default: false },
-        { name: "facebook", value: "#3b5998", default: false },
-    ]),
-);
+        theme: {
+            brandTitle: "Storybook React",
+        },
+
+}});
+addParameters({ backgrounds: [
+    { name: "default", value: "transparent", default: true },
+    { name: "#000", value: "#000000", default: false },
+    { name: "#333", value: "#333333", default: false },
+    { name: "twitter", value: "#00aced", default: false },
+    { name: "facebook", value: "#3b5998", default: false },
+] });
 addDecorator(withKnobs);
 
 /**
