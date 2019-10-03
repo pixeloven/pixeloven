2c9e2bc0cc064fa82b96e05e14174c93dd570df5
Brian Gebel <bgebel@gofundme.com>
2019-10-03 16:13:11 -0700

    refactor(#156): update example to use lib

--- packages/examples/component-library-example/src/atoms/Logo/Logo.stories.tsx
+++ packages/examples/component-library-example/src/atoms/Logo/Logo.stories.tsx
@@ -1,6 +1,5 @@
 import Readme from "./README.md";
 
-import { boolean, text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Logo from "./Logo";
--- packages/examples/component-library-example/src/modules.d.ts
+++ packages/examples/component-library-example/src/modules.d.ts
@@ -1,4 +1,5 @@
 /**
  * Declare file formats not covered by typescript automatically
  */
+declare module "*.md";
 declare module "*.svg";
