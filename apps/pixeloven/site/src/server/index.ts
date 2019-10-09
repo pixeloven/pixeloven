9ce8492dfe15cc25039b49ea916144c89b6782be
Brian Gebel <bgebel@gofundme.com>
2019-10-09 13:43:34 -0700

    fix(#154): lots of updates

--- apps/pixeloven/site/src/server/index.ts
+++ apps/pixeloven/site/src/server/index.ts
@@ -3,17 +3,35 @@ import { config } from "./config";
 import server from "./server";
 
 /**
+ * @description catch unhandled errors and log them to console and exit purposefully. Any errors logged here should be fixed ASAP.
+ */
+process.on("uncaughtException", error => {
+    console.log(error);
+    process.exit(1);
+});
+
+/**
  * Create express application
  * @type {Function}
  */
 const app = express();
-server(app, config);
+server(app, config)
+    .then(() => {
+        /**
+         * Start express server on specific host and port for production builds only.
+         * @description Need to export for development server to work
+         */
+        if (config.environment.node === "production") {
+            app.listen(config.server.port, config.server.host);
+        }
+    })
+    .catch(error => {
+        /**
+         * @todo UC-61 improve server error logging
+         * @description This catches and exits only if the express
+         */
+        console.error(error);
+        process.exit(1);
+    });
 
-/**
- * Start express server on specific host and port
- * @description Need to export for development server to work
- */
-if (config.environment.node === "production") {
-    app.listen(config.server.port, config.server.host);
-}
 export default app;
