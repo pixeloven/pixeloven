0573bf92bbfe79c5f335d101b872dbfdf74615f4
Brian Gebel <bgebel@gofundme.com>
2019-11-14 19:13:28 -0800

    Deploy website version based on 56852ea35384de9a5b3544ff068d8770ab28b022

--- tools/gh-pages/docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"1":"External module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"main\"","url":"modules/_main_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"main","url":"modules/_main_.html#main","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"main\""},{"id":2,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"}]};
