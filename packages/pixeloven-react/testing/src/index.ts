0666c9d2224cb1287584ff51fb23925fc3bc055b
Brian Gebel <bgebel@gofundme.com>
2020-01-10 10:42:34 -0800

    fix: adding note

--- packages/pixeloven-react/testing/src/index.ts
+++ packages/pixeloven-react/testing/src/index.ts
@@ -1,6 +1,9 @@
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+/**
+ * @todo Need to implement this everywhere?
+ */
 Enzyme.configure({
     adapter: new Adapter(),
 });
