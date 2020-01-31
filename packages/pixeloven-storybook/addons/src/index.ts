bb46fee8f7f438734722ebb9c9535df7b0c70c0b
Brian Gebel <bgebel@gofundme.com>
2020-01-31 08:34:20 -0800

    refactor: cleanup comments

--- packages/pixeloven-storybook/addons/src/index.ts
+++ packages/pixeloven-storybook/addons/src/index.ts
@@ -3,13 +3,4 @@ import * as Backgrounds from "@storybook/addon-backgrounds";
 import * as Knobs from "@storybook/addon-knobs";
 import * as Viewport from "@storybook/addon-viewport";
 
-/**
- * @todo Create some helpers and use this to help build stories more efficiently
- * @todo Also can we change the default order of the pages?
- * @todo Does storybook have an official readme package that might be better?
- *
- * @todo move the addons here instead of in common that way config just imports both?
- * @should common be a peer dep instead?
- * @should can we create a common for webpack too so we have less there as well?
- */
 export { Actions, Backgrounds, Knobs, Viewport };
--- packages/pixeloven-storybook/common/src/index.ts
+++ packages/pixeloven-storybook/common/src/index.ts
@@ -3,4 +3,7 @@
  */
 import storybook from "@storybook/react/standalone";
 
+/**
+ * @todo Should this package be peerDep for the others?
+ */
 export default storybook;
