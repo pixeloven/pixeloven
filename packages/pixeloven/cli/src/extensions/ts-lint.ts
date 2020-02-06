017053af45b83cffaca4060471c3d656a5e3c0a4
Brian Gebel <bgebel@gofundme.com>
2020-02-06 15:21:09 -0800

    fix: hotfix for finding file

--- packages/pixeloven/cli/src/extensions/ts-lint.ts
+++ packages/pixeloven/cli/src/extensions/ts-lint.ts
@@ -1,18 +1,30 @@
 import { resolvePath } from "@pixeloven-core/filesystem";
 import { PixelOvenToolbox } from "../types";
 
-const fileName = "tslint.json";
+const tsLintFileName = "tslint.json";
+const tsConfigFileName = "tsconfig.json";
 
 export default (context: PixelOvenToolbox) => {
     async function tsLint(args: string[]) {
         const { print, pixelOven } = context;
-        const configPath = resolvePath(fileName, false);
+        const tsLintPath = resolvePath(tsLintFileName, false);
+        const tsConfigPath = resolvePath(tsConfigFileName, false);
         const tslintArgs = ["tslint", "-t", "codeFrame"];
-        if (configPath) {
-            tslintArgs.concat(["--config", configPath, ...args]);
+        if (tsLintPath) {
+            if (tsConfigPath) {
+                tslintArgs.concat([
+                    "--config",
+                    tsLintPath,
+                    "--project",
+                    tsConfigPath,
+                    ...args,
+                ]);
+            } else {
+                tslintArgs.concat(["--config", tsLintPath, ...args]);
+            }
         } else {
             print.warning(
-                `Unable to find "${fileName}" reverting to default configuration`,
+                `Unable to find "${tsLintPath}" reverting to default configuration`,
             );
             tslintArgs.concat(args);
         }
