08c93c40776c7124fc9a8183cc22f3e3e09917d3
Brian Gebel <bgebel@gofundme.com>
2020-02-06 16:22:39 -0800

    fix: more fixes

--- packages/pixeloven/cli/src/extensions/ts-lint.ts
+++ packages/pixeloven/cli/src/extensions/ts-lint.ts
@@ -2,30 +2,14 @@ import { resolvePath } from "@pixeloven-core/filesystem";
 import { PixelOvenToolbox } from "../types";
 
 const tsLintFileName = "tslint.json";
-const tsConfigFileName = "tsconfig.json";
 
 export default (context: PixelOvenToolbox) => {
     async function tsLint(args: string[]) {
         const { print, pixelOven } = context;
         const tsLintPath = resolvePath(tsLintFileName, false);
-        const tsConfigPath = resolvePath(tsConfigFileName, false);
         let tslintArgs = ["tslint", "-t", "codeFrame"];
         if (tsLintPath) {
-            if (tsConfigPath) {
-                tslintArgs = tslintArgs.concat([
-                    "--config",
-                    tsLintPath,
-                    "--project",
-                    tsConfigPath,
-                    ...args,
-                ]);
-            } else {
-                tslintArgs = tslintArgs.concat([
-                    "--config",
-                    tsLintPath,
-                    ...args,
-                ]);
-            }
+            tslintArgs = tslintArgs.concat(["--config", tsLintPath, ...args]);
         } else {
             tslintArgs = tslintArgs.concat(args);
             print.warning(
