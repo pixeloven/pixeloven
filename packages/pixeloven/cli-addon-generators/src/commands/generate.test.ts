a1c56f2ee09c3a4eef1576acc4a5ead4c0797b8f
Brian Gebel <bgebel@gofundme.com>
2020-02-07 19:39:43 -0800

    feat: adding a bit more testing to the generators

--- packages/pixeloven/cli-addon-generators/src/commands/generate.test.ts
+++ packages/pixeloven/cli-addon-generators/src/commands/generate.test.ts
@@ -1,22 +1,27 @@
 import "jest";
 
-/**
- * @todo This might need to be it's own module so we can use in the addons
- * @todo Need to figure out how to make this work with addons
- */
-import { build, filesystem, print } from "gluegun";
+import { build, print, prompt } from "gluegun";
 import { resolve } from "path";
 import sinon from "sinon";
 
 const cli = build()
     .brand("pixeloven")
-    .src(resolve(__dirname))
+    .src(resolve(__dirname, ".."))
     .create();
 
 const Sandbox = sinon.createSandbox();
 
-import generateModule from "./generate";
+const Stub = {
+    print: Sandbox.stub(print),
+    process: {
+        exit: Sandbox.stub(process, "exit"),
+    },
+    prompt: Sandbox.stub(prompt),
+};
 
+/**
+ * @todo Can we mock the extensions for now to at least get coverage on the command logic?
+ */
 describe("@pixeloven/cli-addon-generators", () => {
     describe("commands", () => {
         describe("webpack", () => {
@@ -26,13 +31,17 @@ describe("@pixeloven/cli-addon-generators", () => {
             afterAll(() => {
                 Sandbox.restore();
             });
-            it("should contains required props", () => {
-                expect(generateModule.name).toEqual("generate");
-                expect(typeof generateModule.run).toEqual("function");
-            });
-            xit("should throw an error without run type", async () => {
+            it("should print info fo existing projects", async () => {
+                Stub.prompt.ask.resolves({
+                    generatorType: "App",
+                });
+
                 const context = await cli.run("generate");
                 expect(context.commandName).toEqual("generate");
+                expect(Stub.prompt.ask.callCount).toEqual(1);
+                expect(Stub.print.info.callCount).toEqual(1);
+                expect(Stub.process.exit.called).toEqual(true);
+                expect(Stub.process.exit.calledWithExactly(0)).toEqual(true);
             });
         });
     });
