b7f8ac0c956ae26c8187677c1a21068539ad1bed
James Wilson <jimmynotjim@me.com>
2020-03-24 14:16:40 -0700

    chore: simplify the package generator

--- packages/pixeloven/cli-addon-generators/src/extensions/createPackage.ts
+++ packages/pixeloven/cli-addon-generators/src/extensions/createPackage.ts
@@ -12,7 +12,8 @@ export default (toolbox: AddonGeneratorsToolbox) => {
             packageRegistry,
             packageVersion,
         } = options;
-        const { template } = toolbox;
+        const { filesystem, plugin, template } = toolbox;
+        const baseDirectory = plugin && plugin.directory;
         const name = packageName;
         const props = {
             authorEmail: packageAuthorEmail,
@@ -24,20 +25,11 @@ export default (toolbox: AddonGeneratorsToolbox) => {
             registry: packageRegistry,
             version: packageVersion,
         };
-        template.generate({
-            props,
-            target: `packages/${name}/jest.json`,
-            template: "package/jest.json.ejs",
-        });
+
         template.generate({
             props,
             target: `packages/${name}/package.json`,
             template: "package/package.json.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/prettier.json`,
-            template: "package/prettier.json.ejs",
         });
         template.generate({
             props,
@@ -46,54 +38,15 @@ export default (toolbox: AddonGeneratorsToolbox) => {
         });
         template.generate({
             props,
-            target: `packages/${name}/stylelint.json`,
-            template: "package/stylelint.json.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/tsconfig.json`,
-            template: "package/tsconfig.json.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/tslint.json`,
-            template: "package/tslint.json.ejs",
-        });
-        template.generate({
-            props,
             target: `packages/${name}/typedoc.json`,
             template: "package/typedoc.json.ejs",
         });
-        template.generate({
-            props,
-            target: `packages/${name}/src/constants.ts`,
-            template: "package/constants.ts.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/src/index.ts`,
-            template: "package/index.ts.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/src/modules.d.ts`,
-            template: "package/modules.d.ts.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/src/types.ts`,
-            template: "package/types.ts.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/src/_mocks_/files.ts`,
-            template: "package/files.ts.ejs",
-        });
-        template.generate({
-            props,
-            target: `packages/${name}/src/_mocks_/styles.ts`,
-            template: "package/styles.ts.ejs",
-        });
+        filesystem.copy(
+            `${baseDirectory}/templates/package`,
+            `packages/${name}`,
+            { matching: ["!*.ejs", "!*.js"], overwrite: true },
+        );
     };
+
     toolbox.createPackage = createPackage;
 };
