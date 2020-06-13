13d1558491043da359687dc504231e3a004a0f38
Brian Gebel <bgebel@gofundme.com>
2020-06-13 10:10:15 -0700

    chore: cleaning up some old files and fixing gen/examples

--- examples/apps/ssr-example/src/shared/components/pages/Home/Home.tsx
+++ examples/apps/ssr-example/src/shared/components/pages/Home/Home.tsx
@@ -3,18 +3,16 @@ import {Helmet} from "react-helmet";
 
 import {UniversalRouteComponentProps} from "@pixeloven-react/routing";
 
-class Home extends React.Component<UniversalRouteComponentProps> {
-    public render(): React.ReactNode {
-        return (
-            <React.Fragment>
-                <Helmet>
-                    <title>Home Page</title>
-                    <meta name="description" content="Home page and stuff" />
-                </Helmet>
-                <p>Hello world</p>
-            </React.Fragment>
-        );
-    }
+function Home(props: UniversalRouteComponentProps) {
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>Home Page</title>
+                <meta name="description" content="Home page and stuff" />
+            </Helmet>
+            <p>Hello world</p>
+        </React.Fragment>
+    );
 }
 
 export default Home;
--- examples/apps/ssr-example/src/shared/components/pages/NoMatch/NoMatch.tsx
+++ examples/apps/ssr-example/src/shared/components/pages/NoMatch/NoMatch.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 import {UniversalRouteComponentProps} from "@pixeloven-react/routing";
 
-class NoMatch extends React.Component<UniversalRouteComponentProps> {
-    public render(): React.ReactNode {
-        return <p>No Match</p>;
-    }
+function NoMatch(props: UniversalRouteComponentProps) {
+    return <p>No Match</p>;
 }
 
 export default NoMatch;
